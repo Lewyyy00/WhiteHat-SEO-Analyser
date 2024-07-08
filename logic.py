deef6322f729efb8c5da09e321bbc65ad611be59
Michal Lewandowski <michalewy00@gmail.com>
2024-07-08 23:08:41 +0200

    fix logic.py

--- logic.py
+++ logic.py
@@ -2,47 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse, parse_qs
 
-'''
-def analyse_site(site='https://miroslawmamczur.pl/beautifulsoup/'):
-    try: 
-        response = requests.get(site)
-        response.raise_for_status() #raise for status pokazuje czy żdaniae się udało
-        return(response.content)
-    except requests.exceptions.RequestException as error:
-            print(f"błąd: {error}") 
-
-
-def find_key_words(promt = 'Najlepsze filmy w Polsce na wieczór - coś z BeautifulSoup'):
-    key_words = promt.split()
-    list_of_key_words = []
-    for i in key_words:
-        list_of_key_words.append(i)
-    return list_of_key_words
-
-#find_key_words()
-
-def find_title(site):
-    try:
-        response = requests.get(site)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        title_tag = soup.title
-        print(title_tag.string)
-    except requests.exceptions.RequestException as error:
-        print(f"błąd: {error}") 
-
-    x = find_key_words(title_tag.string)
-    y = find_key_words()
-    for i in x:
-        found_match = False #flaga pozwalająca śledzić czy dane słowo zostało już użyte
-        for j in y:
-            if i == j:
-               print(f"Słowo kluczowe '{i}' występuje w tytule")
-               found_match = True
-               break
-        if not found_match:
-            print(f"Słowo kluczowe '{i}' nie występuje w tytule")
-'''
-
 def find_headings(site):
     response = requests.get(site)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -50,13 +9,12 @@ def find_headings(site):
     for i in headings:
         print(i.get_text())
 
-
 class WebCrawler:
     def __init__(self,site):
         self.site = site
 
     def find_all_links(self,site):
-        response = requests.get(site)
+        response = requests.get(self.site)
         soup = BeautifulSoup(response.content, 'html.parser')
         links = soup.find_all('a', href = True) #szuka wszytskich linków, gdzie jest spełniony warunek href = true
        
@@ -77,12 +35,13 @@ class WebCrawler:
             print(f"błąd: {error}") 
             return None
 
-    def find_key_words(self,promt = 'wazdan'):
-        key_words = promt.split()
-        list_of_key_words = []
-        for i in key_words:
-            list_of_key_words.append(i)
-        return list_of_key_words
+    def find_key_words(self,prompt):
+        key_words = prompt.split()
+        #list_of_key_words = []
+        #for i in key_words:
+            #list_of_key_words.append(i)
+        #return list_of_key_words
+        return key_words
     
     def analyse_title_and_key_words(self):
         title = self.find_title(self.site)
@@ -99,8 +58,6 @@ class WebCrawler:
                 if not found_match:
                     print(f"Słowo kluczowe '{keyword_2}' nie występuje w tytule")
 
-
-#must fix
 def find_key_words_in_url(site, keywords='wazdan news press losowe'):
     crawler = WebCrawler(site)
     key_words = crawler.find_key_words(keywords) 
@@ -115,21 +72,17 @@ def find_key_words_in_url(site, keywords='wazdan news press losowe'):
                 found_keywords.append(keyword)
     return list(set(found_keywords))
 
-#x = find_key_words_in_url('https://wazdan.com/news/in-the-press/tragaperrasweb-interview-with-joanna-zdanowska-bieniek-the-secret-to-the-success-of-the-coins-series-and-expectations-for-score-the-jackpot')
-#print(x)
-
-#must fix
 def find_key_words_in_all_urls(site, keywords='wazdan market games gaming'):
     crawler = WebCrawler(site)
     urls = crawler.find_all_links(site)
     key_words = crawler.find_key_words(keywords)
-
-    found_keywords = []
+    
+    #found_keywords = []
     for url in urls:
-        found_keywords.append(find_key_words_in_url(url,keywords))
+        keywords = find_key_words_in_url(url,keywords)
         print(f"URL: {url}")
         for keyword in key_words:
-            if keyword in found_keywords:
+            if keyword in keywords:
                     print(f"Słowo kluczowe '{keyword}' jest w URL.")
             else:
                     print(f"Słowo kluczowe '{keyword}' NIE jest w URL.")
@@ -138,16 +91,6 @@ def find_key_words_in_all_urls(site, keywords='wazdan market games gaming'):
 y = find_key_words_in_all_urls('https://wazdan.com/')    
 print(y)
     
-
-def find_headings(site):
-    response = requests.get(site)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    headings = soup.find_all(['h1','h2','h3','h4','h5','h6'])
-    for i in headings:
-        print(i.get_text())
-
-#y = find_headings('https://wazdan.com/news/in-the-press/tragaperrasweb-interview-with-joanna-zdanowska-bieniek-the-secret-to-the-success-of-the-coins-series-and-expectations-for-score-the-jackpot')
-
 '''
 if __name__ == '__main__':
     base_site = 'https://wazdan.com/' 
