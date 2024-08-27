779b1faab180f0cde305be69063b2e95889234e4
Michal Lewandowski <michalewy00@gmail.com>
2024-08-27 20:47:37 +0200

    added is_each_file_loaded()

--- SEO_pro/WebsiteTimeAndSecurity.py
+++ SEO_pro/WebsiteTimeAndSecurity.py
@@ -9,6 +9,7 @@ class PageLoadTimerAnaylyser:
     def __init__(self):
         options = Options()
         options.add_argument("--incognito")
+        options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
         self.service = Service(ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=self.service)
     
@@ -23,10 +24,24 @@ class PageLoadTimerAnaylyser:
         load_time = end_time - start_time
         return load_time
     
+    def open_page(self, url):
+        self.driver.get(url)
+        time.sleep(5)
+
     def is_each_file_loaded(self, url):
-        pass
+        logs = self.driver.get_log("performance")
+        loaded = True
+
+        for log in logs:
+            message = log['message']
+            if "Network.responseReceived" in message:
+                if '"status":404' in message or '"status":500' in message:
+                    loaded = False
+                    print ('not loaded file')
+                    break
         
-    
+        return loaded
+
     def close_browser(self):
         self.driver.quit()
 
@@ -34,6 +49,10 @@ class PageLoadTimerAnaylyser:
 if __name__ == "__main__":
     timer = PageLoadTimerAnaylyser()
     url = 'https://www.screamingfrog.co.uk/seo-spider/user-guide/tabs/#pagespeed'
-    load_time = timer.is_each_file_loaded(url)
+    """load_time = timer.is_each_file_loaded(url)
     print(f"time needed to load the webiste: {load_time:.2f} sec")
+    timer.close_browser()"""
+
+    
+    timer.open_page(url)
     timer.close_browser()
