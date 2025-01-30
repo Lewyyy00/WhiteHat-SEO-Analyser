66df4b84121f6d02cbd6aa647a355744fda2f200
Michal Lewandowski <michalewy00@gmail.com>
2025-01-30 10:42:53 +0100

    add tests for responses in /choose endpoint

--- tests/test_all_and_choose.py
+++ tests/test_all_and_choose.py
@@ -24,5 +24,31 @@ def test_all_success(client):
     data = response.get_json()
 
     #Check the presence of keys in the returned response
-    assert "all" in data
-    assert "all with keywords" in data
+    assert "headings" in data[0]
+    assert "title" in data[0]
+
+def test_choose_success(client):
+
+    """Test for a successful request to the /choose endpoint."""
+    
+    #headings is not accepted
+    options = ['title', 'metadescription', 'content', 'altcontent', 'urlcontent']
+    for option in options:
+        print(option)
+        response = client.post('/choose', json={
+            "url": "https://example.com",
+            "option": f'{option}'
+        })
+        assert response.status_code == 200  
+
+        data = response.get_json()
+
+        #Check the presence of keys in the returned response
+        assert isinstance(data, list)
+        assert len(data) == 2
+        assert "Missing value" in data[0]
+        assert "Multiple values" in data[0]
+        assert "Number of characters" in data[0]
+        assert "Text" in data[0]
+        #assert "kewords density" in data[1]
+        #assert "keywords" in data[1]
